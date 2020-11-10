4b1759b031a946316bcfeaf9215d1d48da3503c9
Beardificent <cis.magito@gmail.com>
2020-11-10 12:14:28 +0100

    construct calls on interface ITransform

--- src/Services/Master.php
+++ src/Services/Master.php
@@ -7,5 +7,12 @@ namespace App\Services;
 class Master
 {
 
-
+        private $transform;
+    /**
+     * Master constructor.
+     */
+    public function __construct(ITransform $transform)
+    {
+        $this->transform = $transform;
+    }
 }
